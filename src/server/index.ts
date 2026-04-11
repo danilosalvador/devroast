43765e63fa233b82a7cb84882fb2d592b1e52fcb
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2026-04-10 21:16:39 -0300

    refactor: migrate from node-http adapter to standalone createHTTPServer for tRPC server initialization

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,34 +1,24 @@
-import { createHTTPHandler } from "@trpc/server/adapters/node-http";
-import { createServer } from "node:http";
+import { createHTTPServer } from "@trpc/server/adapters/standalone";
 import { appRouter } from "./routers/_app";
 import { createTRPCContext } from "./trpc";
 
-const handler = createHTTPHandler({
+const server = createHTTPServer({
   router: appRouter,
   createContext: createTRPCContext,
-});
+  middleware: (req, res, next) => {
+    // CORS for development
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
 
-const server = createServer((req, res) => {
-  // CORS for development
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
+    if (req.method === "OPTIONS") {
+      res.writeHead(200);
+      res.end();
+      return;
+    }
 
-  if (req.method === "OPTIONS") {
-    res.writeHead(200);
-    res.end();
-    return;
-  }
-
-  // Handle tRPC requests
-  if (req.url?.startsWith("/trpc")) {
-    const url = new URL(req.url, `http://${req.headers.host}`);
-    req.url = url.pathname.replace("/trpc", "");
-    handler(req, res);
-  } else {
-    res.writeHead(404);
-    res.end();
-  }
+    next();
+  },
 });
 
 server.listen(3000, () => {
